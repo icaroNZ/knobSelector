bc5937bf78ae3a0aa0e0b385bf4493f73dc9a7a1
Icaro Lavrador <icarolavrador@gmail.com>
2019-06-02 21:47:29 +1200

    Initial Commit

---
+++ RotaryWheel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RotaryWheel
+//
+//  Created by Icaro Lavrador on 2/06/19.
+//  Copyright © 2019 Icaro Lavrador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
